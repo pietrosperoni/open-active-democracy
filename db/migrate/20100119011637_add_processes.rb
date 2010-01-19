dfe9ab74a9e15410421e827f2119efac66679722
rbjarnason <robert@decyphermedia.com>
2010-01-19 01:20:46 +0000

    Migration pre table renaming

---
+++ db/migrate/20100119011637_add_processes.rb
@@ -0,0 +1,164 @@
+class AddProcesses < ActiveRecord::Migration
+  def self.up
+    create_table "case_discussions", :force => true do |t|
+      t.datetime "meeting_date"
+      t.string   "external_id"
+      t.string   "external_link"
+      t.integer  "stage_sequence_number"
+      t.integer  "sequence_number"
+      t.datetime "to_time"
+      t.datetime "from_time"
+      t.string   "transcript_url"
+      t.string   "listen_url"
+      t.string   "meeting_info"
+      t.string   "meeting_type"
+      t.string   "meeting_url"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.integer  "case_id"
+      t.boolean  "processed_for_speech_videos"
+      t.boolean  "published"
+      t.boolean  "in_video_processing",         :default => false
+      t.boolean  "video_processing_complete",   :default => false
+      t.boolean  "has_modified_durations",      :default => false
+    end
+  
+    create_table "case_documents", :force => true do |t|
+      t.datetime "external_date"
+      t.string   "external_id"
+      t.string   "external_link"
+      t.integer  "stage_sequence_number"
+      t.integer  "sequence_number"
+      t.string   "external_type"
+      t.string   "external_author"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.integer  "case_id"
+    end
+  
+    create_table "case_speech_master_videos", :force => true do |t|
+      t.string   "url"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.boolean  "in_processing", :default => false
+      t.boolean  "published",     :default => false
+    end
+  
+    add_index "case_speech_master_videos", ["url"], :name => "index_case_speech_master_videos_on_url", :unique => true
+  
+    create_table "case_speech_videos", :force => true do |t|
+      t.integer  "case_discussion_id"
+      t.string   "title"
+      t.datetime "to_time"
+      t.datetime "from_time"
+      t.integer  "sequence_number"
+      t.integer  "parent_id"
+      t.integer  "case_speech_master_video_id"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.time     "start_offset"
+      t.time     "duration"
+      t.boolean  "in_processing",               :default => false
+      t.boolean  "published",                   :default => false
+      t.integer  "modified_duration_s"
+      t.boolean  "has_checked_duration",        :default => false
+    end
+  
+    create_table "case_types", :force => true do |t|
+      t.string   "case_type"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+    end
+  
+    create_table "cases", :force => true do |t|
+      t.string   "external_link"
+      t.string   "external_name"
+      t.integer  "case_type_id"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.text     "info_1"
+      t.text     "info_2"
+      t.text     "info_3"
+      t.string   "presenter"
+      t.integer  "external_id"
+      t.boolean  "archived",      :default => false
+    end
+  
+    create_table "categories", :force => true do |t|
+      t.string   "name"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+    end
+  
+    create_table "document_comments", :force => true do |t|
+      t.integer  "document_id"
+      t.text     "comment"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.float    "bias"
+      t.integer  "user_id"
+      t.integer  "document_element_id"
+      t.integer  "case_speech_video_id"
+    end
+  
+    create_table "document_elements", :force => true do |t|
+      t.integer  "sequence_number"
+      t.integer  "document_id"
+      t.integer  "parent_id"
+      t.binary   "content",           :limit => 2147483647
+      t.binary   "content_text_only", :limit => 2147483647
+      t.string   "content_number"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.integer  "user_id"
+      t.integer  "content_type"
+      t.boolean  "original_version"
+    end
+  
+    create_table "document_states", :force => true do |t|
+      t.string   "state"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+    end
+  
+    create_table "document_types", :force => true do |t|
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.string   "document_type"
+    end
+  
+    create_table "documents", :force => true do |t|
+      t.integer  "category_id"
+      t.integer  "issue_id"
+      t.integer  "document_state_id"
+      t.integer  "document_type_id"
+      t.datetime "voting_close_time"
+      t.boolean  "published"
+      t.string   "external_name"
+      t.string   "external_author"
+      t.string   "external_state"
+      t.datetime "external_creation_date"
+      t.string   "external_link"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.boolean  "document_frozen"
+      t.integer  "user_id"
+      t.integer  "case_id"
+      t.integer  "case_document_id"
+      t.boolean  "original_version"
+    end
+  
+    create_table "ratings", :force => true do |t|
+      t.integer  "rating",                      :default => 0
+      t.datetime "created_at",                                  :null => false
+      t.integer  "rateable_id",                 :default => 0,  :null => false
+      t.integer  "user_id",                     :default => 0,  :null => false
+      t.string   "rateable_type", :limit => 50, :default => "", :null => false
+    end
+  
+    add_index "ratings", ["user_id"], :name => "fk_ratings_user"  
+  end
+
+  def self.down
+  end
+end
