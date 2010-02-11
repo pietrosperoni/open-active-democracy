ba5bc04e8f7ffb435eee557476deba17215a1cc6
rbjarnason <robert@decyphermedia.com>
2010-02-11 05:43:57 +0000

    Added hoptoad

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,3 +31,10 @@ end
 deploy.task :start do
 # nothing
 end
+
+
+Dir[File.join(File.dirname(__FILE__), '..', 'vendor', 'gems', 'hoptoad_notifier-*')].each do |vendored_notifier|
+  $: << File.join(vendored_notifier, 'lib')
+end
+
+require 'hoptoad_notifier/capistrano'
---
+++ config/initializers/hoptoad.rb
@@ -0,0 +1,3 @@
+HoptoadNotifier.configure do |config|
+  config.api_key = '144468394a5403a00a72563c0045f759'
+end
