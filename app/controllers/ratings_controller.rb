f851cf87fd18cff5388f5626e0fd2f4d4b9c7ad1
rbjarnason <robert@decyphermedia.com>
2010-02-12 05:39:09 +0000

    Get user id to ratings

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -14,6 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
  
 class RatingsController < ApplicationController
+  before_filter :login_required
   before_filter :get_class_by_name
 
   def rate
