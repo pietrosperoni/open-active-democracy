7123405fb67f2a2b48cb4b73a54d5d665a3ceb5d
rbjarnason <robert@decyphermedia.com>
2010-02-12 04:39:13 +0000

    Localizations

--- app/models/user_publisher.rb
+++ app/models/user_publisher.rb
@@ -71,8 +71,8 @@ class UserPublisher < Facebooker::Rails::Publisher
   end
 
   def point_template
-    one_line_story_template "{*actor*} added a <a href='{*point_url*}'>talking point</a> to <a href='{*priority_url*}'>{*priority_name*}</a> at <a href='{*government_url*}'>{*government_name*}</a>"
-    short_story_template "{*actor*} added a <a href='{*point_url*}'>talking point</a> to <a href='{*priority_url*}'>{*priority_name*}</a> at <a href='{*government_url*}'>{*government_name*}</a>", render(:partial => "title_and_body")
+    one_line_story_template "{*actor*} bætti við <a href='{*point_url*}'>rökum</a> við <a href='{*priority_url*}'>{*priority_name*}</a> á <a href='{*government_url*}'>{*government_name*}</a>"
+    short_story_template "{*actor*} bætti við <a href='{*point_url*}'>rökum</a> við <a href='{*priority_url*}'>{*priority_name*}</a> á <a href='{*government_url*}'>{*government_name*}</a>", render(:partial => "title_and_body")
     action_links action_link("Skoða betur","{*point_url*}")      
   end 
   
