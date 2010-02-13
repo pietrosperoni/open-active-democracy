214abda909d93772f570cc21ca8fafaf7742943e
rbjarnason <robert@decyphermedia.com>
2010-02-13 04:21:55 +0000

    Reverted

--- public/javascripts/skuggathing.js
+++ public/javascripts/skuggathing.js
@@ -1,12 +1,14 @@
 jQuery(document).ready(function() {
 	if (location.pathname.substring(1,19) != 'priority_processes') {
 		jQuery(".althingi_process_container").addClass("hidden");
-		jQuery(".althingi_process_container").children("div").hide();
-		jQuery(".althingi_process_container").children("p").children("span").text("sjá");
+		jQuery(".althingi_process_container").children("span.showorhide").text("sjá");
+		foobar();
 	}
 	else {
 		jQuery(".althingi_process_container").addClass("shown");
-		jQuery(".althingi_process_container").children("p").children("span").text("fela");
+		jQuery(".althingi_process_container p span").text("fela");
+		jQuery(".althingi_process_container p span").text("fela");
+		foobar();
 	}
 	
 	jQuery(".althingi_process_container h2, .althingi_process_container p").css("cursor","pointer");
@@ -16,8 +18,6 @@ jQuery(document).ready(function() {
 			jQuery(this).siblings().slideDown();
 			jQuery(this).siblings().children("span.showorhide").text("fela");
 			jQuery(this).children("span.showorhide").text("fela");
-			jQuery(this).parent().addClass("shown");
-			jQuery(this).parent().removeClass("hidden");
 			foobar();
 		});
 		jQuery(".althingi_process_container.shown h2, .althingi_process_container.shown p").click(function() {
