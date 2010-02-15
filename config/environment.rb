2f8401af88fbca778f257898bf7580a6af8acfc7
rbjarnason <robert@decyphermedia.com>
2010-02-15 17:42:22 +0000

    Fix for solr

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,8 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
+ENV['WEBSOLR_URL']="http://localhost:8080/solr"
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
