856877635298c0920b0338248ee55545bcf46972
Robert Bjarnason <robert.bjarnason@gmail.com>
2010-09-29 15:47:49 +0200

    Chapters

--- app/controllers/treaty_documents_controller.rb
+++ app/controllers/treaty_documents_controller.rb
@@ -2,14 +2,7 @@ class TreatyDocumentsController < ApplicationController
   
   layout "esb_treaty_documents"
   
-  def TreatyDocumentsController
-    
-  end
   def index
-     #@all_documents =  TreatyDocument.all.group_by(&:chapter) 
-     respond_to do |format|
-        format.html { render :action => "index" }
-    end   
   end  
 end
 
