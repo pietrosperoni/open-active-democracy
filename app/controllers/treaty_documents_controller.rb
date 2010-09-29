ce570161c817d746bf1eefa3b684e1ed60e7d0e6
Robert Bjarnason <robert.bjarnason@gmail.com>
2010-09-29 15:52:48 +0200

    Chapters

--- app/controllers/treaty_documents_controller.rb
+++ app/controllers/treaty_documents_controller.rb
@@ -3,6 +3,10 @@ class TreatyDocumentsController < ApplicationController
   layout "esb_treaty_documents"
   
   def index
-  end  
+  end
+
+  def show
+    @all_treaty_documents_for_chapter_and_status = TreatyDocument.find(:all, :conditions=>["chapter = ? AND negotiation_status = ?",params[:chapter_id], params[:negotiation_status]])
+  end
 end
 
