b8611133ccc2a8cdf54de7a2fa985546a31585b9
Robert Bjarnason <robert.bjarnason@gmail.com>
2010-10-27 11:05:12 +0200

    X

---
+++ lib/questions_email_setup.rb
@@ -0,0 +1,10 @@
+QUESTION_EMAILS_SETUP=[[[22],"robert@ibuar.is,gunnar@ibuar.is,jon.sverrisson@utn.stjr.is"],
+                      [[1,5,7,8,12,15,18,20],"robert@ibuar.is,gunnar@ibuar.is,jon.sverrisson@utn.stjr.is"],
+                      [[2,3,4,6,9,10,14,19,21,25,26,27,28],"robert@ibuar.is,gunnar@ibuar.is,jon.sverrisson@utn.stjr.is"],
+                      [[16,29,32,33],"robert@ibuar.is,gunnar@ibuar.is,jon.sverrisson@utn.stjr.is"],
+                      [[23,24],"robert@ibuar.is,gunnar@ibuar.is,jon.sverrisson@utn.stjr.is"],
+                      [[35],"robert@ibuar.is,gunnar@ibuar.is,jon.sverrisson@utn.stjr.is"],
+                      [[11],"robert@ibuar.is,gunnar@ibuar.is,jon.sverrisson@utn.stjr.is"],
+                      [[17],"robert@ibuar.is,gunnar@ibuar.is,jon.sverrisson@utn.stjr.is"],
+                      [[13],"robert@ibuar.is,gunnar@ibuar.is,jon.sverrisson@utn.stjr.is"],
+                      [[30,31],"robert@ibuar.is,gunnar@ibuar.is,jon.sverrisson@utn.stjr.is"]]
