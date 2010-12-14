b013ebd0627ea63e03c0450939276554dfc79f2b
rbjarnason <robert@decyphermedia.com>
2010-12-14 17:32:19 +0000

    Fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,10 +19,6 @@ config.action_controller.perform_caching             = true
 # Disable delivery errors, bad email addresses will be ignored
 config.action_mailer.raise_delivery_errors = true
 
-config.action_mailer.smtp_settings = {
-  :enable_starttls_auto => false
-}
-
 #config.action_mailer.smtp_settings = {
 #  :address => "smtp.sendgrid.net",
 #  :port => "25",
