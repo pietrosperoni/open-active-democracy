855ffc5bfd8bdf74a2deb77f4339cd7d110dd757
rbjarnason <robert@decyphermedia.com>
2010-12-15 18:21:17 +0000

    Change to HTTPS

--- vendor/plugins/facebooker2/lib/facebooker2/rails/helpers/javascript.rb
+++ vendor/plugins/facebooker2/lib/facebooker2/rails/helpers/javascript.rb
@@ -26,7 +26,6 @@ module Facebooker2
                 cookie : #{cookie}, // enable cookies to allow the server to access the session
                 xfbml  : #{xfbml}  // parse XFBML
               });
-              FB._domain.staticfb = 'https://static-s.ak.facebook.com/';
               #{extra_js}
             };
 
