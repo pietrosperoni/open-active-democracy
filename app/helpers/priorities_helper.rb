72cd93a52e05be6c121db682fdc79f6f6dca43f7
Robert Bjarnason <rbjarnason>
2011-02-25 23:38:18 +0000

    Fixing comments

--- app/helpers/priorities_helper.rb
+++ app/helpers/priorities_helper.rb
@@ -24,9 +24,9 @@ module PrioritiesHelper
   
   def get_points_number_text(points_count, total_points,new_points=false)
     if points_count>0
-      "<span style=\"color:#666;font-size:0.75em;\">(#{points_count} af #{total_points})</span>"
+      "<span style=\"color:#666;font-size:0.75em;\">(#{points_count} af #{total_points})</span>".html_safe
     else
-      "<span style=\"color:#666;font-size:0.75em;\">(#{new_points ? t(:new_points) : t(:no_points)})</span>"
+      "<span style=\"color:#666;font-size:0.75em;\">(#{new_points ? t(:new_points) : t(:no_points)})</span>".html_safe
     end
   end
 end
