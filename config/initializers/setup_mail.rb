8454e42b301b219fabe34edb34f7614a10efe89e
Robert Bjarnason <rbjarnason>
2011-03-23 14:22:51 +0000

    Email fixes

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,15 +1,25 @@
+ActionMailer::Base.smtp_settings = {  
+  :address              => "localhost",  
+  :enable_starttls_auto => false
+} 
 
-class DevelopmentMailInterceptor  
-  def self.delivering_email(message)  
-    message.subject = "[#{message.to}] #{message.subject}"  
-    message.to = "robert"  
-  end  
-end
 
 if Rails.env.development?
-  ActionMailer::Base.smtp_settings = {  
-    :address              => "localhost",  
-    :enable_starttls_auto => false
-  } 
+  class DevelopmentMailInterceptor  
+    def self.delivering_email(message)  
+      message.subject = "#{message.to} #{message.subject}"  
+      message.to = "robert"  
+    end  
+  end
+
+  Mail.register_interceptor(DevelopmentMailInterceptor)
+elsif Rails.env.production?
+  class DevelopmentMailInterceptor  
+    def self.delivering_email(message)  
+      message.subject = "#{message.to} #{message.subject}"  
+      message.to = "robert@ibuar.is,gunnar@ibuar.is"  
+    end  
+  end
+
   Mail.register_interceptor(DevelopmentMailInterceptor)
 end
