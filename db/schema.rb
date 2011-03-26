1198355dd77d33952d68e1413966755722bd77d0
Robert Bjarnason <rbjarnason>
2011-03-26 04:53:36 +0000

    Partner ids for categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110326034010) do
+ActiveRecord::Schema.define(:version => 20110326044956) do
 
   create_table "activities", :force => true do |t|
     t.integer  "user_id"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(:version => 20110326034010) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "partner_id"
   end
 
   create_table "changes", :force => true do |t|
