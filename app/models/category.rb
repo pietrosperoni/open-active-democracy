4f16ad8b89cc6cd83bfb9041157a86a449d06338
Robert Bjarnason <rbjarnason>
2011-03-26 04:57:31 +0000

    Partner ids for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,12 @@ class Category < ActiveRecord::Base
   def i18n_name
     tr(self.name, "model/category")
   end
+  
+  def self.for_partner
+    if Partner.current and Category.where(:partner_id=>Partner.current.id).count > 0
+      Category.where(:partner_id=>Partner.current.id).order("name")
+    else
+      Category.order("name")
+    end
+  end
 end
