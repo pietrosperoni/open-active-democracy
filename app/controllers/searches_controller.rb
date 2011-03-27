a2921a456a1642f188b0bd7e559aeace5ed5f50d
Robert Bjarnason <rbjarnason>
2011-03-27 01:40:35 +0000

    Enable wildcard searches as default in search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,13 +6,13 @@ class SearchesController < ApplicationController
     if params[:q]
       @query = params[:q]
       @page_title = tr("Search {government_name} priorites for '{query}'", "controller/searches", :government_name => current_government.name, :query => @query)
-      @facets = ThinkingSphinx.facets params[:q], :all_facets => true, :with => {:partner_id => Partner.current ? Partner.current.id : 0}, :page => params[:page]
+      @facets = ThinkingSphinx.facets params[:q], :all_facets => true, :with => {:partner_id => Partner.current ? Partner.current.id : 0}, :star => true, :page => params[:page]
       if params[:category_name]
         @search_results = @facets.for(:category_name=>params[:category_name])
       elsif params[:class]
         @search_results = @facets.for(:class=>params[:class].to_s)
       else
-        @search_results = ThinkingSphinx.search params[:q], :with => {:partner_id => Partner.current ? Partner.current.id : 0}, :retry_stale => true, :page => params[:page]
+        @search_results = ThinkingSphinx.search params[:q], :with => {:partner_id => Partner.current ? Partner.current.id : 0}, :star => true, :retry_stale => true, :page => params[:page]
       end
     end
     respond_to do |format|
