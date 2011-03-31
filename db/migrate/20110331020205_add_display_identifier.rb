7ea14268bf527aa26ad96a368c14be972ca5b33b
Robert Bjarnason <rbjarnason>
2011-03-31 14:50:02 +0000

    Make id url unique

--- db/migrate/20110331020205_add_display_identifier.rb
+++ db/migrate/20110331020205_add_display_identifier.rb
@@ -1,7 +1,7 @@
 class AddDisplayIdentifier < ActiveRecord::Migration
   def self.up
     add_column :users, :identifier_url, :string
-    add_index :users, :identifier_url
+    add_index :users, :identifier_url, :unique => true
   end
 
   def self.down
