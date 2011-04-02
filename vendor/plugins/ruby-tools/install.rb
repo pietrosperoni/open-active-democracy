9d16c5dfea0c79df8df93347c02506b58cfbeedb
Robert Bjarnason <rbjarnason>
2011-04-02 14:38:52 +0000

    Removed unused plugins

--- vendor/plugins/ruby-tools/install.rb
+++
@@ -1,5 +0,0 @@
-require 'fileutils'
-
-File.cp File.join(File.dirname(__FILE__), 'sample.yml'),  File.join(File.dirname(__FILE__), '..', '..', '..', 'config', 'trans.yml')
-puts IO.read(File.join(File.dirname(__FILE__), 'README'))
-
