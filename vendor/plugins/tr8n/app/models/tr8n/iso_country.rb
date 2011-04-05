1e6812ab557d1181a4e52515557d07b8cbea1b37
Robert Bjarnason <rbjarnason>
2011-04-05 03:25:28 +0000

    Other language selection

--- vendor/plugins/tr8n/app/models/tr8n/iso_country.rb
+++ vendor/plugins/tr8n/app/models/tr8n/iso_country.rb
@@ -1,6 +1,8 @@
 class Tr8n::IsoCountry < ActiveRecord::Base
   set_table_name :tr8n_iso_countries
   has_and_belongs_to_many :languages, :class_name => 'Tr8n::Language', :foreign_key => "tr8n_iso_country_id", :association_foreign_key=>"tr8n_language_id", :join_table=>"tr8n_iso_countries_tr8n_languages"
+  scope :by_name, :order=>"country_english_name"
+  belongs_to :partner
 
   EU_AND_EEA_COUNTRIES = ["at", "be", "bg", "cy", "cz", "dk", "ee", "fi", "fr", "de", "gr", "hu", "ie", "it", "lv", "lt", "lu", "mt", "nl", "pl", "pt", "ro", "sk", "si", "es", "se", "gb","is","no","li","ch"]
 
@@ -22,5 +24,29 @@ class Tr8n::IsoCountry < ActiveRecord::Base
   
   def base_flag_url(size)
     "flags/#{size}/#{self.code.downcase}.png"
+  end
+  
+  def self.all_countries_in_columns
+    one = []
+    two = []
+    three = []
+    four = []
+    flip = 1
+    self.by_name.all.each do |a|
+      if flip==1
+        one << a
+        flip = 2
+      elsif flip==2
+        two << a
+        flip = 3
+      elsif flip==3
+        three << a
+        flip = 4
+      elsif flip==4
+        four << a
+        flip = 1
+      end
+    end
+    [one,two,three,four]
   end
 end
