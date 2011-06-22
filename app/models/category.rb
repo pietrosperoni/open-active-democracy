25b00a6d79cffb5c1b1afca10989d9afce3dc14b
Robert Bjarnason <rbjarnason>
2011-06-22 14:41:09 +0000

    Various fixes

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,12 +1,16 @@
 class Category < ActiveRecord::Base
   has_many :priorities
-  has_attached_file :icon, :styles => { :icon_25 => "25x25#", :icon_50  => "50x50#", :icon_100 => "100x100#" }
-    
+  has_attached_file :icon, :styles => { :icon_32 => "32x32#", :icon_25 => "25x25#", :icon_50  => "50x50#", :icon_100 => "100x100#" }
+
   validates_attachment_size :icon, :less_than => 5.megabytes
   validates_attachment_content_type :icon, :content_type => ['image/png']
 
   def i18n_name
     tr(self.name, "model/category")
+  end
+  
+  def to_url
+    "/issues/#{i18n_name.parameterize_full[0..60]}"
   end
   
   def self.for_partner
