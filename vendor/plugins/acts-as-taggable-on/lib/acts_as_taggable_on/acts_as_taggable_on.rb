f2c734d4e1e56eff5b5644c5d367ce5a3d30f463
Robert Bjarnason <rbjarnason>
2011-09-12 15:56:37 +0000

    Fix for tags and tag debug

--- vendor/plugins/acts-as-taggable-on/lib/acts_as_taggable_on/acts_as_taggable_on.rb
+++ vendor/plugins/acts-as-taggable-on/lib/acts_as_taggable_on/acts_as_taggable_on.rb
@@ -294,7 +294,8 @@ module ActiveRecord
               base_tags.delete(*old_tags) if old_tags.any?
               new_tag_names.each do |new_tag_name|
                 new_tag = Tag.find_or_create_with_like_by_name(new_tag_name)
-                unless Tagging.where(:tag_id=>new_tag ? new_tag.id : nil, :context=> tag_type, :taggable_id=>self.id, :tagger_id=>owner ? owner.id : nil)
+                puts "new_tag: #{new_tag.inspect} self_id: #{self.id}"
+                unless Tagging.where(:tag_id=>new_tag ? new_tag.id : nil, :context=> tag_type, :taggable_id=>self.id, :tagger_id=>owner ? owner.id : nil).first
                   Tagging.create(:tag_id => new_tag.id, :context => tag_type,
                                  :taggable => self, :tagger => owner)
                 else
