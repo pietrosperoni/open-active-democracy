de56bc7895eccffcf6dddf6895b11631b5743762
Robert Bjarnason <rbjarnason>
2011-10-17 20:03:14 +0000

    Fixes for category description

--- db/migrate/20111017185935_add_description_to_categories.rb
+++ db/migrate/20111017185935_add_description_to_categories.rb
@@ -1,7 +1,8 @@
 class AddDescriptionToCategories < ActiveRecord::Migration
   def self.up
     add_column :categories, :description, :text, :default=>""
-    Category.transaction do |category|
+    Category.reset_column_information
+    Category.all.each do |category|
       category.description = ""
       category.save
     end
