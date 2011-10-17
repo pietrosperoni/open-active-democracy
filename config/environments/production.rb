5302b6d4fb1db88df60620d84ad5c99504574df5
Robert Bjarnason <rbjarnason>
2011-10-17 21:40:56 +0000

    Remove deprication errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ OpenActiveDemocracy::Application.configure do
   # just comment this out and Rails will serve the files
 
   # See everything in the log (default is :info)
-  config.log_level = :debug
+  config.log_level = :info
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
@@ -45,7 +45,8 @@ OpenActiveDemocracy::Application.configure do
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
+  #config.active_support.deprecation = :notify
+  config.active_support.deprecation.silenced = true
   
   config.cache_store = :dalli_store, '127.0.0.1:11211', { :namespace => "oad_3_#{Rails.env}_#{Rails.application.config.database_configuration[Rails.env]["git_branch"]}", 
                                                           :compress => true, :compress_threshold => 64*1024 }
