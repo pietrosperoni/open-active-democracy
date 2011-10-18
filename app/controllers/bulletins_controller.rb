pietrosperoni/open-active-democracy

commit 0f542d24fb34675a8c4929e5c9a954807adafb2a

Fix allowing you to create discussion threads on your own profile

app/controllers/bulletins_controller.rb
@@ -22,12 +22,11 @@ class BulletinsController < ApplicationController
   end
 
   def create
-    params[:activity][:user_id] = current_user.id
-    if params[:activity][:other_user_id] # this is a post to another person's profile
-      @activity = ActivityBulletinProfileNew.create(:user => User.find(params[:activity][:other_user_id]), :other_user => current_user)
-      ActivityBulletinProfileAuthor.create(:activity => @activity, :user => current_user, :other_user => @activity.user, :is_user_only => true)
-    else
+    if params[:activity][:user_id] == current_user.id
       @activity = ActivityBulletinNew.create(params[:activity])
+    else # this is a post to another person's profile
+      @activity = ActivityBulletinProfileNew.create(:user => User.find(params[:activity][:user_id]), :other_user => current_user)
+      ActivityBulletinProfileAuthor.create(:activity => @activity, :user => current_user, :other_user => @activity.user, :is_user_only => true)
     end
     @comment = @activity.comments.new(params[:comment])
     @comment.user = current_user

app/views/bulletins/_form.html.erb
@@ -44,8 +44,8 @@
 							<% if @document %>
 								<input type="hidden" name="activity[document_id]" value="<%= @document.id %>">
 							<% end %>
-							<% if @user and @user != current_user %>
-								<input type="hidden" name="activity[other_user_id]" value="<%= @user.id %>">
+							<% if @user %>
+								<input type="hidden" name="activity[user_id]" value="<%= @user.id %>">
 							<% end %>												
 					</td>
 				</tr>

commit f645af85f9c70b7315a878033ae2294d3ee8e541

Fix discussion threads on priorities
Commit 0f542d2 broke them. Now they both work.

app/controllers/bulletins_controller.rb
@@ -22,11 +22,13 @@ class BulletinsController < ApplicationController
   end
 
   def create
-    if params[:activity][:user_id] == current_user.id
-      @activity = ActivityBulletinNew.create(params[:activity])
-    else # this is a post to another person's profile
-      @activity = ActivityBulletinProfileNew.create(:user => User.find(params[:activity][:user_id]), :other_user => current_user)
+    params[:activity] ||= {}
+    params[:activity][:user_id] = current_user.id
+    if params[:activity][:other_user_id] # this is a post to another person's profile
+      @activity = ActivityBulletinProfileNew.create(:user => User.find(params[:activity][:other_user_id]), :other_user => current_user)
       ActivityBulletinProfileAuthor.create(:activity => @activity, :user => current_user, :other_user => @activity.user, :is_user_only => true)
+    else
+      @activity = ActivityBulletinNew.create(params[:activity])
     end
     @comment = @activity.comments.new(params[:comment])
     @comment.user = current_user

app/views/bulletins/_form.html.erb
@@ -44,8 +44,8 @@
 							<% if @document %>
 								<input type="hidden" name="activity[document_id]" value="<%= @document.id %>">
 							<% end %>
-							<% if @user %>
-								<input type="hidden" name="activity[user_id]" value="<%= @user.id %>">
+							<% if @user and @user != current_user %>
+								<input type="hidden" name="activity[other_user_id]" value="<%= @user.id %>">
 							<% end %>												
 					</td>
 				</tr>