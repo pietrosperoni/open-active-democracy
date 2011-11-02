4a2907819dbad7225a401aa20bf1bec8fa94dab5
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2011-11-02 19:00:58 +0000

    Put simple_format in the right place
    Putting it last will cause links to be filtered out

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -177,10 +177,10 @@ class Comment < ActiveRecord::Base
   
   auto_html_for(:content) do
     html_escape
+    simple_format
     youtube(:width => 330, :height => 210)
     vimeo(:width => 330, :height => 180)
     link :target => "_blank", :rel => "nofollow"
-    simple_format
   end
   
 end
