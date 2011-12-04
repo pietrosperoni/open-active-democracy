f5597210d9827d3f9ca97a7e35bf1fcf78b5b0ad
Robert Bjarnason <rbjarnason>
2011-12-04 14:37:38 +0000

    Fixes for betri hverfi

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -8,7 +8,7 @@ class Partner < ActiveRecord::Base
   
   belongs_to :picture
   
-  has_attached_file :logo, :styles => { :icon_96 => "96x96#", :icon_140 => "140x140#", :icon_214_32 => "214x32#", :icon_107_16 => "107x16#", :icon_53_8 => "53x8#", :icon_180 => "180x180#", :medium  => "450x" }
+  has_attached_file :logo, :styles => { :icon_96 => "96x96#", :icon_140 => "140x140#", :icon_340_74 => "340x74#", :icon_214_32 => "214x32#", :icon_107_16 => "107x16#", :icon_53_8 => "53x8#", :icon_180 => "180x180#", :medium  => "450x" }
     
   validates_attachment_size :logo, :less_than => 5.megabytes
   validates_attachment_content_type :logo, :content_type => ['image/jpeg', 'image/png', 'image/gif']
